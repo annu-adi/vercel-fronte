16243d68114383ab71ef9f02c5c7392673b45b97
annu-adi <rashmi.maurya10@gmail.com>
2026-07-10 19:15:56 +0530

    Update API endpoint for fetching user data

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
    useEffect(() =>{
     const fetchUser = async () =>{
       try{
-        const response = await axios.get("http://localhost:4000/api/user/patient/me", {withCredentials:true});
+        const response = await axios.get("https://vercel-backend-bqw8.vercel.app/v1/user/patient/me", {withCredentials:true});
         setIsAuthenticated(true);
         setUser(response.data.user);
 
