15835cc56fddab0ae54430c200ec23dea0f50106
annu-adi <rashmi.maurya10@gmail.com>
2026-07-10 19:17:50 +0530

    Update login API endpoint to production URL

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
     e.preventDefault();
     try{
       const response = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://vercel-backend-bqw8.vercel.app/v1/user/login",
         {email, password, confirmPassword, role:"Patient"},
         {
       withCredentials:true,
