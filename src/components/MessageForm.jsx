2083b009dc17a2ca424fd73e7d2d0a6c84fcfaf9
annu-adi <rashmi.maurya10@gmail.com>
2026-07-11 19:52:29 +0530

    Update MessageForm.jsx

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -20,7 +20,7 @@ const MessageForm =()=>{
   try{
     await axios
     .post(
-     "https://vercel-backeee.vercel.app/v1/message/send",
+     "https://vercel-backeee.vercel.app/api/v1/message/send",
       {firstName, lastName, phone,address, email, message},
       {
         withCredentials:true,
